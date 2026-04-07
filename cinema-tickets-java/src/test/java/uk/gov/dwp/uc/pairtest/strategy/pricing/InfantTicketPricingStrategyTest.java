5c6d6ff2e13df21ccf0f8ddb43c65060e4ffa5f6
Jason Roy <jroy_998@hotmaail.com>
2026-04-07 14:15:05 +0100

    Further unit tests

---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/strategy/pricing/InfantTicketPricingStrategyTest.java
@@ -0,0 +1,23 @@
+package uk.gov.dwp.uc.pairtest.strategy.pricing;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InfantTicketPricingStrategyTest {
+
+    private InfantTicketPricingStrategy infantTicketPricingStrategy;
+
+    @BeforeEach
+    void beforeEach() {
+        infantTicketPricingStrategy = new InfantTicketPricingStrategy();
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "0, 0", "1, 0", "2, 0", "10, 0" })
+    void infantTicketPriceWillBeCalculated(int numberOfTickets, int expectedPrice) {
+        assertEquals(expectedPrice, infantTicketPricingStrategy.calculatePrice(numberOfTickets));
+    }
+}
---
+++ cinema-tickets-java/src/test/java/uk/gov/dwp/uc/pairtest/strategy/validation/account/AccountIdValidatorTest.java
@@ -0,0 +1,32 @@
+package uk.gov.dwp.uc.pairtest.strategy.validation.account;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import uk.gov.dwp.uc.pairtest.exception.AccountIdInvalidException;
+import uk.gov.dwp.uc.pairtest.validation.account.AccountIdValidator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AccountIdValidatorTest {
+
+    private AccountIdValidator accountIdValidator;
+
+    @BeforeEach
+    void beforeEach() {
+        accountIdValidator = new AccountIdValidator();
+    }
+
+    @ParameterizedTest
+    @ValueSource (longs = { 0L, 1L, 12L, 123L, 54321L })
+    void validAccountIdWillPassValidation(Long accountId) {
+        assertDoesNotThrow(() -> accountIdValidator.validate(accountId));
+    }
+
+    @ParameterizedTest
+    @ValueSource (longs = {-1L, -123L, -54321L})
+    void invalidAccountIdWillThrowException(Long accountId) {
+        assertThrows(AccountIdInvalidException.class, () -> accountIdValidator.validate(accountId));
+    }
+}
